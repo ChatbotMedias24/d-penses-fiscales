856951bd5567a6e77435c2a350d353fb076a029d
Chatbot <134295092+ChatbotMedias24@users.noreply.github.com>
2024-10-23 11:58:29 +0100

    Update cha.py

--- cha.py
+++ cha.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import openai
+import streamlit as st
 from dotenv import load_dotenv
 import pickle
 from PyPDF2 import PdfReader
@@ -23,8 +25,18 @@ import toml
 import docx2txt
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 from langchain.callbacks.base import BaseCallbackHandler
+import docx2txt
+from dotenv import load_dotenv
 if 'previous_question' not in st.session_state:
     st.session_state.previous_question = []
+
+# Chargement de l'API Key depuis les variables d'environnement
+load_dotenv(st.secrets["OPENAI_API_KEY"])
+
+# Configuration de l'historique de la conversation
+if 'previous_questions' not in st.session_state:
+    st.session_state.previous_questions = []
+
 st.markdown(
     """
     <style>
@@ -95,7 +107,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
 # Sidebar contents
 textcontainer = st.container()
 with textcontainer:
@@ -110,37 +121,28 @@ questions = [
         "Quelles sont les principales mesures fiscales supprimées dans le cadre des réformes de l'IS et de la TVA ?",
         "Quels sont les principaux objectifs du système fiscal de référence présenté dans le rapport ?",      
         "Comment le rapport évalue-t-il l'impact des dépenses fiscales sur le budget de l'État ?",
-        "Comment le rapport aborde-t-il les incitations fiscales en matière de développement durable ou d'innovation ?"
+        """Comment le rapport aborde-t-il les incitations fiscales en matière de développement durable ou d'innovation ?"""
 
     ]    
- 
 load_dotenv(st.secrets["OPENAI_API_KEY"])
-conversation_history = StreamlitChatMessageHistory()
-
+# Initialisation de l'historique de la conversation dans `st.session_state`
+if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = StreamlitChatMessageHistory()
 def main():
     conversation_history = StreamlitChatMessageHistory()  # Créez l'instance pour l'historique
-    st.header("PLF2025: Explorez le rapport sur les dépenses fiscales 💬")
+
+    st.header("PLF2025: Explorez le rapport sur les dépenses fiscales à travers notre chatbot 💬")
     
     # Load the document
     docx = 'PLF2025-depenses-fiscales_Fr.docx'
     
     if docx is not None:
+        # Lire le texte du document
         text = docx2txt.process(docx)
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(text=text)
 
-        embeddings = OpenAIEmbeddings()
-        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        with open("aaa.pkl", "wb") as f:
-            pickle.dump(VectorStore, f)
-
+        # Afficher toujours la barre de saisie
         st.markdown('<div class="input-space"></div>', unsafe_allow_html=True)
         selected_questions = st.sidebar.radio("****Choisir :****", questions)
-
         # Afficher toujours la barre de saisie
         query_input = st.text_input("", key="text_input_query", placeholder="Posez votre question ici...", help="Posez votre question ici...")
         st.markdown('<div class="input-space"></div>', unsafe_allow_html=True)
@@ -153,44 +155,62 @@ def main():
         else:
             query = ""
 
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
+        if query :
+            st.session_state.conversation_history.add_user_message(query)  # Ajouter à l'historique
 
-            llm = OpenAI(model="gpt-3.5-turbo-instruct")
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                if "Donnez-moi un résumé du rapport" in query:
-                    response="Le rapport évalue l'impact des dépenses fiscales sur le budget de l'État en utilisant la méthode de la perte initiale en recette. Cette approche consiste à chiffrer, de manière ex-post, la réduction des recettes fiscales résultant de l'adoption d'une dépense fiscale, en supposant que cette adoption n'affecte pas le comportement des contribuables. En d'autres termes, il s'agit d'estimer l'écart par rapport au système fiscal de référence pour quantifier les recettes perdues, en considérant que toutes les transactions auraient eu lieu même sans la mesure adoptée. Cette méthode permet de se concentrer sur les pertes fiscales directes, tout en laissant la possibilité d'utiliser des estimations plus sophistiquées lorsque les informations disponibles le permettent ."
-                # Votre logique pour traiter les réponses
-                conversation_history.add_user_message(query)
-                conversation_history.add_ai_message(response)
+        
+            messages = [
+                {
+                    "role": "user",
+                    "content": (
+                        f"{query}. En tenant compte du texte suivant essayer de ne pas dire le texte ne contient pas les informations si ne trouve pas à partir de texte répondre d'aprés votre connaissance stp et ne dire pas stp le texte incomplet ou incompréhensible essayer de formulé une bon réponse sans critiquer le texte par exemple ne pas dire texte fragmenter ou quelque chose comme ça répondre directement stp parceque je vais l'afficher o lecteur: {text} "
+                    )
+                }
+            ]
+
+            # Appeler l'API OpenAI pour obtenir le résumé
+            response = openai.ChatCompletion.create(
+                model="gpt-4o-mini",
+                messages=messages
+            )
+
+            # Récupérer le contenu de la réponse
+
+            summary = response['choices'][0]['message']['content']
+            if "Donnez-moi un résumé du rapport" in query:
+                summary="Le rapport évalue l'impact des dépenses fiscales sur le budget de l'État en utilisant la méthode de la perte initiale en recette. Cette approche consiste à chiffrer, de manière ex-post, la réduction des recettes fiscales résultant de l'adoption d'une dépense fiscale, en supposant que cette adoption n'affecte pas le comportement des contribuables. En d'autres termes, il s'agit d'estimer l'écart par rapport au système fiscal de référence pour quantifier les recettes perdues, en considérant que toutes les transactions auraient eu lieu même sans la mesure adoptée. Cette méthode permet de se concentrer sur les pertes fiscales directes, tout en laissant la possibilité d'utiliser des estimations plus sophistiquées lorsque les informations disponibles le permettent ."
+            st.session_state.conversation_history.add_ai_message(summary)  # Ajouter à l'historique
+            
+            # Afficher la question et le résumé de l'assistant
+            #conversation_history.add_user_message(query)
+            #conversation_history.add_ai_message(summary)
 
             # Format et afficher les messages comme précédemment
             formatted_messages = []
-            previous_role = None  # Variable pour stocker le rôle du message précédent
-            for msg in conversation_history.messages:
-                role = "user" if msg.type == "human" else "assistant"
-                avatar = "🧑" if role == "user" else "🤖"
-                css_class = "user-message" if role == "user" else "assistant-message"
+            previous_role = None 
+            if st.session_state.conversation_history.messages: # Variable pour stocker le rôle du message précédent
+                    for msg in conversation_history.messages:
+                        role = "user" if msg.type == "human" else "assistant"
+                        avatar = "🧑" if role == "user" else "🤖"
+                        css_class = "user-message" if role == "user" else "assistant-message"
 
-                if role == "user" and previous_role == "assistant":
-                    message_div = f'<div class="{css_class}" style="margin-top: 25px;">{msg.content}</div>'
-                else:
-                    message_div = f'<div class="{css_class}">{msg.content}</div>'
+                        if role == "user" and previous_role == "assistant":
+                            message_div = f'<div class="{css_class}" style="margin-top: 25px;">{msg.content}</div>'
+                        else:
+                            message_div = f'<div class="{css_class}">{msg.content}</div>'
 
-                avatar_div = f'<div class="avatar">{avatar}</div>'
+                        avatar_div = f'<div class="avatar">{avatar}</div>'
                 
-                if role == "user":
-                    formatted_message = f'<div class="message-container user"><div class="message-avatar">{avatar_div}</div><div class="message-content">{message_div}</div></div>'
-                else:
-                    formatted_message = f'<div class="message-container assistant"><div class="message-content">{message_div}</div><div class="message-avatar">{avatar_div}</div></div>'
+                        if role == "user":
+                            formatted_message = f'<div class="message-container user"><div class="message-avatar">{avatar_div}</div><div class="message-content">{message_div}</div></div>'
+                        else:
+                            formatted_message = f'<div class="message-container assistant"><div class="message-content">{message_div}</div><div class="message-avatar">{avatar_div}</div></div>'
                 
-                formatted_messages.append(formatted_message)
-                previous_role = role  # Mettre à jour le rôle du message précédent
+                        formatted_messages.append(formatted_message)
+                        previous_role = role  # Mettre à jour le rôle du message précédent
 
-            messages_html = "\n".join(formatted_messages)
-            st.markdown(messages_html, unsafe_allow_html=True)
+                    messages_html = "\n".join(formatted_messages)
+                    st.markdown(messages_html, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
